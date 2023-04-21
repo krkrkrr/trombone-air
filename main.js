1bbf21a093af5e06e7dffc77cbfe44a7183c372a
krr <67230121+cool-omb@users.noreply.github.com>
2023-04-22 08:55:47 +0900

    [add] Success play sound
    Test AudioContext.

---
+++ main.js
@@ -0,0 +1,34 @@
+var audioContext = new AudioContext()
+
+window.onload = () => {
+  document.body.addEventListener('click', () => {
+    console.log('on click')
+    if (audioContext.state === 'running') {
+      audioContext.suspend().then(() => {
+        console.log('Resume context')
+      })
+    } else if (audioContext.state === 'suspended') {
+      audioContext.resume().then(() => {
+        console.log('Suspend context')
+        play(261)
+      })
+    }
+  })
+}
+
+//..........................
+function play(hz) {
+  //..........................
+  // window.AudioContext = window.AudioContext || window.webkitAudioContext
+
+  var osc = audioContext.createOscillator()
+  osc.frequency.value = hz
+  var audDes = audioContext.destination
+  osc.connect(audDes)
+  osc.start = osc.start || osc.noteOn
+  osc.start()
+
+  setTimeout(function () {
+    osc.stop(0)
+  }, 200)
+}
